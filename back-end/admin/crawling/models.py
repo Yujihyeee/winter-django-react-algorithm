80860774d496b9841cc671c55a3900f78c6d47c9
Yujihyeee <rladnddl@naver.com>
2021-10-18 15:25:16 +0900

    15

--- back-end/admin/crawling/models.py
+++ back-end/admin/crawling/models.py
@@ -1,5 +1,8 @@
 import csv
 import datetime as dt
+import re
+
+import matplotlib.pyplot as plt
 import nltk
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -7,6 +10,8 @@ from konlpy.tag import Okt
 from admin.common.models import ValueObject
 from selenium import webdriver
 from nltk.tokenize import word_tokenize
+from nltk import FreqDist
+from wordcloud import WordCloud
 
 
 class Crawling(object):
@@ -14,17 +19,43 @@ class Crawling(object):
         pass
 
     def process(self):
-        nltk.download()
+        # nltk.download()
         vo = ValueObject()
         vo.context = 'admin/crawling/data/'
-        # self.samsung_report(vo)
+        self.samsung_report(vo)
 
     def samsung_report(self, vo):
         okt = Okt()
+        daddy_bag = okt.pos('아버지 가방에 들어가신다', norm=True, stem=True)
+        print(daddy_bag)
         okt.pos('삼성전자 글로벌센터 전자사업부', stem=True)
-        with open('admin/crawling/data/kr-Report_2018.txt', 'r', encoding='UTF-8') as f:
-            texts = f.read()
-        print(texts)
+        with open(f'{vo.context}kr-Report_2018.txt', 'r', encoding='UTF-8') as f:
+            full_texts = f.read()
+        line_removed_texts = full_texts.replace('\n', ' ')
+        tokenizer = re.compile(r'[^ ㄱ-힣]+')
+        tokenized_texts = tokenizer.sub('', line_removed_texts)
+        tokens = word_tokenize(tokenized_texts)
+        noun_tokens = []
+        for token in tokens:
+            token_pos = okt.pos(token)
+            noun_token = [txt_tag[0] for txt_tag in token_pos if txt_tag[1] == 'Noun']
+            if len(''.join(noun_token)) > 1:
+                noun_tokens.append(''.join(noun_token))
+        noun_token_join = ' '.join(noun_tokens)
+        tokens = word_tokenize(noun_token_join)
+        stopfile = f'{vo.context}stopwords.txt'
+        with open(stopfile, 'r', encoding='UTF-8') as f:
+            stopwords = f.read()
+        stopwords = stopwords.split(' ')
+        stopwords.extend(['용량', '각주', '가능보고서', '고려', '전세계', '릴루미노', '가치창'])
+        texts_without_stopwords = [text for text in tokens if text not in stopwords]
+        freq_texts = pd.Series(dict(FreqDist(texts_without_stopwords))).sort_values(ascending=False)
+        wcloud = WordCloud(f'{vo.context}D2Coding.ttf', relative_scaling=0.2,
+                           background_color='white').generate(' '.join(texts_without_stopwords))
+        plt.figure(figsize=(12, 12))
+        plt.imshow(wcloud, interpolation='bilinear')
+        plt.axis('off')
+        plt.savefig(f'{vo.context}wcloud.png')
 
     def naver_movie(self):
         vo = ValueObject()
@@ -85,11 +116,11 @@ class Crawling(object):
                     break
                 last_height = new_height
 
-        '''trump_df = pd.DataFrame(columns=['id', 'message'])
-        number = 1
-        for i in range(len(total_tweets)):
-            for j in range(len(total_tweets[i])):
-                trump_df = trump_df.append({'id':number, 'message': (total_tweets[i][j]).text},
-                                           ignore_index=True)
-                number = number + 1
-        print(trump_df.head())'''
+'''trump_df = pd.DataFrame(columns=['id', 'message'])
+number = 1
+for i in range(len(total_tweets)):
+    for j in range(len(total_tweets[i])):
+        trump_df = trump_df.append({'id':number, 'message': (total_tweets[i][j]).text},
+                                   ignore_index=True)
+        number = number + 1
+print(trump_df.head())'''
